d5a3a8967e4aa61b26ff16addd48e9ad08ffed44
f4263 <fulvio.russo@guest.italiaonline.it>
2018-05-02 18:56:14 +0200

    fix for screen orientation

--- qml/Utility.js
+++ qml/Utility.js
@@ -54,7 +54,7 @@
 
     /* Depending on the Pagewidht of the Page (ie: the Device type) decide the Height of the scrollable */
     function getContentHeight(){
-  console.log("-- landscapeWindow:----------- ");
+  
         if(rootPage.width > units.gu(110))
             return configurationPage.height + configurationPage.height/2 + units.gu(20)
         else
